48329fd44e9c758bae6d0fe1a93380a50f3e76fc
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-29 08:42:31 -0500

    First commit. Fresh Laravel 7 install. Add chart.js horizontal bar chart and pie chart served from public and from CDN.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Fall 2020 Freshman Profile</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    </head>
+    <body>
+      <h1>2 - FTFT F1 by Gender</h1>
+      <canvas id="hbc"></canvas>
+      <canvas id="pc"></canvas>
+
+      {{-- <canvas id="pc" width="1600" height="900"></canvas> --}}
+
+      {{-- <canvas id="hbc" width="1600" height="900"></canvas> --}}
+      {{-- <canvas id="pc" width="1600" height="900"></canvas> --}}
+
+
+      <script src="/js/ccsj-sample-charts.js"></script>
+    </body>
+</html>
