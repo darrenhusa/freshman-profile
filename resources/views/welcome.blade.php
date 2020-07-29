67e618649daeed99b563632e6f3c2c2205abb0dc
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-29 18:08:27 -0500

    Config Vue successfully. Test ExampleComponent. npm install chart.js Create PieChartComponent shell.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,24 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Fall 2020 Freshman Profile</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+        {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script> --}}
     </head>
     <body>
       <h1>2 - FTFT F1 by Gender</h1>
-      <canvas id="hbc"></canvas>
-      <canvas id="pc"></canvas>
+
+      <div id="app">
+        <example-component></example-component>
+      </div>
+
+      {{-- <canvas id="hbc"></canvas> --}}
+      {{-- <canvas id="pc"></canvas>         --}}
 
       {{-- <canvas id="pc" width="1600" height="900"></canvas> --}}
 
       {{-- <canvas id="hbc" width="1600" height="900"></canvas> --}}
       {{-- <canvas id="pc" width="1600" height="900"></canvas> --}}
 
-
-      <script src="/js/ccsj-sample-charts.js"></script>
+      {{-- <script src="/js/ccsj-sample-charts.js"></script> --}}
+      <script src="/js/app.js"></script>
     </body>
 </html>
