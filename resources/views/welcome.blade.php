01145c3c9958ac5e98462bf7a3a08bf7874223e1
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-07-29 23:59:07 -0500

    Copy Freshman Profile "layout" code to facilitate completion of the full Fall 2020 report.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,16 @@
         {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script> --}}
     </head>
     <body>
-      <h1>2 - FTFT F1 by Gender</h1>
-      <h3>Headcounts</h3>
-
       <!-- TODO - Fix how to set the chart size????!!!! -->
       <!-- ref = https://stackoverflow.com/questions/37621020/setting-width-and-height -->
       <!-- style="position: relative; width:600vw; height:400vh;" -->
 
       <div id="app">
+        <h1>1 - F1 Headcounts by FT/PT Status and Entry-types</h1>
+
+        <h1>2 - FTFT F1 by Gender</h1>
+        <h3>Headcounts</h3>
+
         <div class="container">
           <horizontal-bar-chart-component :labels="['Female', 'Male']"
                                           :values="[71, 53]">
@@ -28,12 +30,31 @@
           </pie-chart-component>
         </div>
 
+        <h1>TEST PIE CHART</h1>
         <h3>Headcounts %</h3>
         <div class="container">
           <pie-chart-component :labels="['Female', 'Male']"
                                :values="[75, 25]">
           </pie-chart-component>
         </div>
+
+  <h1>3 - FTFT F1 by Ethnicity</h1>
+
+  {{-- <h1>4 - FTFT F1 by Religion</h1> --}}
+
+  {{-- <h1>5 - FTFT F1 by Athletic Status</h1> --}}
+
+  {{-- <h1>6 - FTFT F1 by Zero EFC Status</h1> --}}
+
+  {{-- <h1>7 - FTFT F1 by Zero EFC Status</h1> --}}
+
+  {{-- <h1>8</h1>
+  <p>FTFT F1 by State of Primary Residence</p>
+  <p>FTFT F1 Indiana Residents by County</p>
+  <p>FTFT F1 Lake County Residents by City</p>
+
+  <h1>9</h1>
+  <p>Top Recruiting Sources for FTFT F1 Students</p> --}}
 
       </div>
 
