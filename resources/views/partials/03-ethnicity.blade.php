3dc5c12f1a049fb8c79ff771cc257da5714cb3f6
Darren Henderson <darrenh@ccsj.edu>
2020-08-24 09:56:50 -0500

    Update ethnicity result to add a note regarding the need to combine 'Other and Unknown'. Add notes to show the disaggregated results. Push updating the horizontal bar chart Vue component to accept a total of 5 datasets to a future update.

--- resources/views/partials/03-ethnicity.blade.php
+++ resources/views/partials/03-ethnicity.blade.php
@@ -4,13 +4,15 @@
   <h3>Headcounts</h3>
 
   <div class="hbc-container">
-    <horizontal-bar-chart-component :labels="['Black or African American', 'Hispanics of any race', 'White', 'Other']"
-                                    :values="[15, 26, 8, 110]">
+    <horizontal-bar-chart-component :labels="['Black or African American', 'Hispanics of any race', 'White', 'Other and Unknown']"
+                                    :values="[13, 25, 7, 116]">
     </horizontal-bar-component>
   </div>
 
   <div class="note mt-5">
-    <h4 class="italic"><span class="underline">Note</span>: FTFT F1 = First-time, Full-time F1 students</h4>
+    <h4 class="italic"><span class="underline">Note</span>: FTFT F1 = First-time, Full-time F1 students</h4><br />
+    <h4 class="italic"><span class="underline">Note</span>: The horizontal bar chart Vue component as initially designed is limited to a <u>total of 4 datasets</u>, therefore as a result the last category combines the "Other and Unknown" groups in order to create a producable chart.</h4>
+    <h4 class="italic"><span class="underline">Note</span>: The "Other and Unknown" category results disaggregated are: Other = 4 (2%) and Unknown = 116 (72%)</h4>
   </div>
 
   <div class="source mt-5">
@@ -19,8 +21,8 @@
 
   <h3>Headcounts %</h3>
   <div class="pc-container">
-    <pie-chart-component :labels="['Black or African American', 'Hispanics of any race', 'White', 'Other']"
-                         :values="[9.4, 16.4, 5.0, 69.2]">
+    <pie-chart-component :labels="['Black or African American', 'Hispanics of any race', 'White', 'Other and Unknown']"
+                         :values="[8, 16, 4, 72]">
     </pie-chart-component>
   </div>
 
@@ -30,8 +32,8 @@
 
   {{-- TODO - Add note about rounding error --}}
 
-  <div class="source mt-5">
+  <!-- <div class="source mt-5">
     <h4 class="italic"><span class="underline">Source</span>: Empower Student Records (SR)</h4>
-  </div>
+  </div> -->
 
 </div>
