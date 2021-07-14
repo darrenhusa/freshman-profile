c3340c9a1104a736663787b58f21f98f3cdb1c53
Darren Henderson <darrenhusa@yahoo.com>
2021-07-14 09:04:33 -0500

    Edits to item 07 formatting.

--- resources/views/partials/07-high-school-gpa.blade.php
+++ resources/views/partials/07-high-school-gpa.blade.php
@@ -23,23 +23,22 @@
           </pie-chart-component>
       </div>
   </div>
-   
+
+  <h3 class="mt-5 text-lg font-semibold">Total with high school GPA = 119</h3>
+  <h3 class="mb-5 text-lg font-semibold">Average high school GPA = 2.63</h3>
+
   <div class="note mt-5">
-    <h4 class="italic"><span class="underline">Note</span>: FTFT F1 = First-time, Full-time F1 students</h4>
+    <h4 class="italic"><span class="underline mr-1">Note:</span>FTFT F1 = First-time, Full-time F1 students</h4>
   </div>
 
-  <div class="source mt-5">
-    <h4 class="italic"><span class="underline">Source</span>: Empower Student Records (SR)</h4>
+  <div class="source mt-2">
+    <h4 class="italic"><span class="underline mr-1">Source:</span>Slate CRM</h4>
   </div>
 
 </div>
 
-
-
-<div class="item-container mt-5">
-  <h1 class="text-xl font-bold">07 - FTFT F1 by High School GPA</h1>
-
-  <h3>Slate Query Definitions</h3>
+<div>
+  <!-- <h3>Slate Query Definitions</h3>
   <ul>
     <li>The FTFT F1 data set is defined in Slate as:</li>
     <li>PersonEntryType = 'High School' for first-time and</li>
@@ -47,44 +46,16 @@
     <li>PersonClassDivision = 'F1' for Freshman (first-semester)</li>
     <li>Using Rank 1 High School (SchoolsTypeLevelofStudy = 'High School' and SchoolsRankByLevelofStudy = '1')</li>
     <li>HS GPA = Slate SchoolsGPAVerified field</li>
-  </ul>
+  </ul> -->
 
-  <h3>Headcounts</h3>
 
-  <div class="hbc-container">
-    <horizontal-bar-chart-component :labels="['1.0 to 2.0', '2.01 to 3.0', '3.01 and above']"
-                                    :values="[26, 60, 33]">
-    </horizontal-bar-component>
-  </div>
-
-  <div class="note mt-5">
-    <h4 class="italic"><span class="underline">Note</span>: FTFT F1 = First-time, Full-time F1 students</h4>
-  </div>
-
-  <div class="source mt-5">
-    <h4 class="italic"><span class="underline">Source</span>: Slate CRM</h4>
-  </div>
-
-  <h3>Headcounts %</h3>
-  <div class="pc-container">
-    <pie-chart-component :labels="['1.0 to 2.0', '2.01 to 3.0', '3.01 and above']"
-                         :values="[21.8, 50.4, 27.7]">
-    </pie-chart-component>
-  </div>
-
-  <h3>Total with high school GPA = 119</h3>
-  <h3>Average high school GPA = 2.63</h3>
-
-  <div class="note mt-5 italic">
+  <!-- <div class="note mt-5 italic">
     <h4><span class="underline">Notes</span>:</h4>
     <ul>
       <li>FTFT F1 = First-time, Full-time F1 students</li>
       <li>There may be some rounding error with the percentages.</li>
     </ul>
-  </div>
+  </div> -->
 
-  <div class="source mt-5">
-    <h4 class="italic"><span class="underline">Source</span>: Slate CRM</h4>
-  </div>
 
 </div>
